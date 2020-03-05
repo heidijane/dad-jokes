828ecaf5968f9e1ecc04b6a391a8077a54ce6b2f
heidijane <heidi@heidisprouse.com>
2020-03-05 15:00:54 -0600

    added styling and wiggle animation to jokes

--- scripts/JokeHTML.js
+++ scripts/JokeHTML.js
@@ -3,8 +3,7 @@
 */
 
 export const createJokeHTML = (joke) => {
-    return `
-        <section id="joke--${joke.id}" class="joke">
+    return `<section id="joke--${joke.id}" class="joke">
             <h4 class="joke__question">Q: ${joke.question}</h4>
             <div class="joke__answer hidden">A: ${joke.answer}</div>
         </section>
